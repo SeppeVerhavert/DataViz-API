76d7fe065b36b53639d3570a07d8ad51b961b836
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-29 13:51:13 +0200

    add pretty print

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "hello world";
+$newArray = [];
+
+for ($x = 0; $x <= 9; $x++) {
+    $newNumber1 = rand(1, 6);
+    $newNumber2 = rand(1, 6);
+    $newNumber3 = rand(1, 6);
+    $newNumber = [$x, $newNumber1 + $newNumber2 + $newNumber3];
+    array_push($newArray, $newNumber);
+}
+
+echo json_encode($newArray, JSON_PRETTY_PRINT);
